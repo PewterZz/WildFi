0222ea17f9b89bd30b7de24b553b9d51ee7d431c
PewterZz <peterns1609@gmail.com>
2024-01-05 09:59:14 +0700

    init

---
+++ Assets/Scripts/Player/BodyHitbox.cs
@@ -0,0 +1,9 @@
+using Fusion;
+
+namespace SimpleFPS
+{
+	public class BodyHitbox : Hitbox
+	{
+		public float DamageMultiplier = 1f;
+	}
+}
---
+++ Assets/Scripts/UI/UIDisconnectedView.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace SimpleFPS
+{
+	public class UIDisconnectedView : MonoBehaviour
+	{
+		public void GoToMenu()
+		{
+			var gameUI = GetComponentInParent<GameUI>(true);
+			gameUI.GoToMenu();
+		}
+
+		private void Update()
+		{
+			// Make sure the cursor stays unlocked.
+			Cursor.lockState = CursorLockMode.None;
+			Cursor.visible = true;
+		}
+	}
+}
